e35c11dc4622b71fdf16e16cbf15786765ba3ccc
Zinaida Razinkova <razinkova.zina@gmail.com>
2021-06-04 13:57:06 +0300

    Adds homework

---
+++ contacts.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const fs = require("fs").promises;
+const shortid = require("shortid");
+
+const contactsPath = path.join("./db/contacts.json");
+const id = shortid.generate();
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const listContacts = JSON.parse(data);
+    return console.table(listContacts);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const listContacts = JSON.parse(data);
+    const contact = listContacts.filter((contact) => contact.id === contactId);
+    return console.table(contact);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const listContacts = JSON.parse(data);
+    const newListContacts = listContacts.filter(
+      (contact) => contact.id != contactId
+    );
+    await fs.writeFile(contactsPath, JSON.stringify(newListContacts), "utf8");
+    return console.table(newListContacts);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: id,
+    name,
+    email,
+    phone,
+  };
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    const listContacts = JSON.parse(data);
+    const contacts = [...listContacts, newContact];
+
+    await fs.writeFile(contactsPath, JSON.stringify(contacts), "utf8");
+    return console.table(contacts);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+module.exports = {
+  contactsPath,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
---
+++ index.js
@@ -0,0 +1,34 @@
+const {
+  contactsPath,
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+} = require("./contacts.js");
+
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      console.log(listContacts());
+      break;
+
+    case "get":
+      getContactById(id);
+      break;
+
+    case "add":
+      addContact(name, email, phone);
+      break;
+
+    case "remove":
+      removeContact(id);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+
+invokeAction(argv);
